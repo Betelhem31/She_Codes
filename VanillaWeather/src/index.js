548c20de1925a8acb555e43000d6149e34a83603
Betelhem <Betelhemmisgan49@gmail.com>
2025-09-23 03:06:04 +0300

    added the search functionality using API

--- VanillaWeather/src/index.js
+++ VanillaWeather/src/index.js
@@ -1,10 +1,23 @@
+function refreshWeather(response) {
+  let temperatureElement = document.querySelector("#temprature");
+  temperatureElement.innerHTML = Math.round(response.data.temperature.current);
+  let cityh1 = document.querySelector("#city");
+  cityh1.innerHTML = response.data.city;
+}
+function searchCity(city) {
+  let apiKey = "ob0b34bc0a25c05530t69fd67a4bbafb";
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
+  //make api call update the ui
+  axios.get(apiUrl).then(refreshWeather);
+}
 function handleSearchSubmit(event) {
   event.preventDefault();
   let SearchInput = document.querySelector("#search-form-input");
-  let cityh1 = document.querySelector("#city");
-  cityh1.innerHTML = `${SearchInput.value}`;
+
+  searchCity(SearchInput.value);
 }
 
 let searchFormElement = document.querySelector("#search-form");
 
 searchFormElement.addEventListener("submit", handleSearchSubmit);
+searchCity("Addis Ababa");
